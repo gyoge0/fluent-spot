082aba7d7fe76b2da1ddb8fbf9991ca3d52e7613
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-06-15 20:32:29 -0700

    Configure oauth sign in via oauth tauri plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,49 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Window;
+use tauri::{command, AppHandle, Wry};
+use tauri_plugin_oauth::{start_with_config, OauthConfig};
+
+#[command]
+async fn do_oauth(handle: AppHandle<Wry>, window: Window<Wry>, url: &str) -> Result<(), String> {
+    // spawn in a new window we use for oauth
+    let oauth_window = tauri::WindowBuilder::new(
+        &handle,
+        "oauth-window",
+        tauri::WindowUrl::External(url.to_string().parse().unwrap()),
+    )
+    .focused(true)
+    .resizable(false)
+    .closable(true)
+    .build()
+    .map_err(|err| err.to_string())?;
+
+    start_with_config(
+        OauthConfig {
+            ports: Some(vec![10000]),
+            response: None,
+        },
+        move |callback| {
+            // close the window and send another event when we get the callback
+            let result = oauth_window.close();
+            if result.is_err() {
+                let _ = dbg!(result.err().unwrap());
+            }
+            let result = window.emit("oauth-received", callback);
+            if result.is_err() {
+                let _ = dbg!(result.err().unwrap());
+            }
+        },
+    )
+    .map_err(|err| err.to_string())?;
+
+    Ok(())
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![do_oauth])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
